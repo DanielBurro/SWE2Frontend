bf4f4a2c7986c926212c02e786c2b07d04c50e33
justinscherf <inf24244@lehre.dhbw-stuttgart.de>
2026-04-01 14:44:02 +0200

    setup ng zorro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { de_DE, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import de from '@angular/common/locales/de';
+
+registerLocaleData(de);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideNzI18n(de_DE),
+  ],
 };
