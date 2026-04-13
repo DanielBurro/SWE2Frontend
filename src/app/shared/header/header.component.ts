44ffa56164e780c4916e370e6ace1dc5543cad74
1337H4XO15 <st.laurin2005@gmail.com>
2026-04-14 01:03:16 +0200

    zurück zu gold

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -17,16 +15,13 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     RouterLink,
-    RouterLinkActive,
     FormsModule,
     NzLayoutModule,
-    NzMenuModule,
     NzButtonModule,
     NzInputModule,
     NzIconModule,
     NzDropDownModule,
     NzAvatarModule,
-    NzDividerModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
@@ -34,5 +29,13 @@ import { FormsModule } from '@angular/forms';
 export class HeaderComponent {
   isLoggedIn = false;
   searchQuery = '';
-  isDarkMode = true;
+
+  getInitials(): string {
+    return 'MK'; // TODO: AuthService
+  }
+
+  onSearch(): void {
+    // TODO: Router navigate to /events?q=searchQuery
+    console.log('Suche:', this.searchQuery);
+  }
 }
